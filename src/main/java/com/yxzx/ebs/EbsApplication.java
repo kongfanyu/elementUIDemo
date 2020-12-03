a05ed879cc16285e62a1790dfb4b55366786aba3
kongfanyu <19388483@qq.com>
2020-12-03 14:57:25 +0800

    添加了注释：完成用户管理模块

--- src/main/java/com/yxzx/ebs/EbsApplication.java
+++ src/main/java/com/yxzx/ebs/EbsApplication.java
@@ -1,6 +1,7 @@
 package com.yxzx.ebs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//ElmentUI 示例代码
 @SpringBootApplication
 public class EbsApplication {
     public static void main(String[] args) {
